06831e7ba62479c61b611e3546b6e8b252bcbfce
DanielC19 <danysam1010@gmail.com>
2025-03-24 19:01:47 -0500

    Feature: Add create queue route

--- mom/src/routes/queue_routes.py
+++ mom/src/routes/queue_routes.py
@@ -4,6 +4,15 @@ from src.controllers.queue_controller import QueueController
 queue_bp = Blueprint('queue', __name__)
 queue_controller = QueueController()
 
+@queue_bp.route('/queue', methods=['POST'])
+def create_queue():
+    data = request.json
+    return queue_controller.create_queue(data)
+
+@queue_bp.route('/queue', methods=['GET'])
+def list_queues():
+    return queue_controller.get_queues()
+
 @queue_bp.route('/queue/<queue_id>', methods=['PUT'])
 def push_message(queue_id):
     data = request.json
@@ -12,7 +21,3 @@ def push_message(queue_id):
 @queue_bp.route('/queue/<queue_id>', methods=['GET'])
 def pull_message(queue_id):
     return queue_controller.pull_message(queue_id)
-
-@queue_bp.route('/queue', methods=['GET'])
-def list_queues():
-    return queue_controller.get_queues()
