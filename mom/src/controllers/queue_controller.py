2d2ac86cd5b22b5a398749110e26098ea2b52934
DanielC19 <danysam1010@gmail.com>
2025-03-24 19:01:59 -0500

    Fix: Queue datatype on response

--- mom/src/controllers/queue_controller.py
+++ mom/src/controllers/queue_controller.py
@@ -9,7 +9,7 @@ class QueueController:
     def create_queue(self, data):
         queue = Queue(data["queue_id"])
         self.queue_service.create_queue(queue)
-        return jsonify(queue), 201
+        return jsonify(queue.to_dict()), 201
 
     def get_queues(self):
         queues = self.queue_service.get_queues()
@@ -19,13 +19,13 @@ class QueueController:
         message = self.queue_service.push_message(data, queue_id)
         if not message:
             return jsonify({"message": "Queue not found"}), 404
-        return jsonify(message), 201
+        return jsonify(message.to_dict()), 201
 
     def pull_message(self, queue_id):
         message = self.queue_service.pull_message(queue_id)
         if not message:
             return jsonify({"message": "Queue not found"}), 404
-        return jsonify(message), 200
+        return jsonify(message.to_dict()), 200
 
 
 
