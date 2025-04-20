e524af18db81ffa58d5f02bb09644963a3800142
DanielC19 <danysam1010@gmail.com>
2025-04-20 14:50:01 -0500

    Refactor: Remove replication proto

--- mom/app.py
+++ mom/app.py
@@ -3,16 +3,14 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import grpc
 from concurrent import futures
-from src.utils import mom_pb2, mom_pb2_grpc, replication_pb2, replication_pb2_grpc
+from src.utils import mom_pb2_grpc
 from kazoo.client import KazooClient
 import socket
 import random
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-import time
 from src.controllers.TopicServiceServicer import TopicServiceServicer
 from src.controllers.QueueServiceServicer import QueueServiceServicer
-from src.controllers.ReplicationServiceServicer import ReplicationServiceServicer
 
 ZOOKEEPER_HOSTS = "127.0.0.1:2181" 
 HOSTS_PATH = "/hosts_service"
@@ -27,7 +25,6 @@ def serve(port):
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     mom_pb2_grpc.add_TopicServiceServicer_to_server(TopicServiceServicer(), server)
     mom_pb2_grpc.add_QueueServiceServicer_to_server(QueueServiceServicer(), server)
-    replication_pb2_grpc.add_ReplicationServiceServicer_to_server(ReplicationServiceServicer(), server)
     server.add_insecure_port("[::]:50051")
     server.add_insecure_port(f"[::]:{port}")
     print(f"gRPC server running on port 50051 and {port}")
